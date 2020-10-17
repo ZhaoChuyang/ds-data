9d0a92aff22df78de017efa1844e750ab6100291
chuyang <zcy201420752@gmail.com>
2020-10-17 19:49:27 +0800

    update lab5 data range

--- conf/lab5.py
+++ conf/lab5.py
@@ -3,7 +3,7 @@ Total 10 test cases
 
 """
 SEED = 10
-NUM_CASES = 15
+NUM_CASES = 5
 
 ADD = 'add_operation'
 RESET = 'reset_operation'
@@ -11,110 +11,162 @@ MULTIPLY = 'multiply_operation'
 TRANSPOSE = 'transpose_operation'
 OUTPUT = 'output_operation'
 
+DENSE_ADD = 'dense_add_operation'
+DENSE_MULTIPLY = 'dense_multiply_operation'
+DENSE_RESET = 'dense_reset_operation'
+
 
 PROBLEM_SETTINGS = [
-    # dict(
-    #     w=50,
-    #     n=30,
-    #     m=30,
-    #     op=[ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD,
-    #         ADD, ADD, ADD, ADD, OUTPUT, OUTPUT, RESET]
-    # ),
-    # dict(
-    #     w=100,
-    #     n=500,
-    #     m=500,
-    #     op=[ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD,
-    #         ADD, ADD, ADD, ADD, OUTPUT, OUTPUT, RESET]
-    # ),
     dict(
+        dense=False,
+        w=20,
+        n=5,
+        m=5,
+        op=[ADD, TRANSPOSE, MULTIPLY, OUTPUT]
+    ),
+    dict(
+        dense=False,
         w=40,
         n=10,
         m=10,
         op=[ADD] * 5 + [RESET] * 2 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=40,
         n=100,
         m=100,
         op=[ADD] * 15 + [RESET] * 2 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=80,
         n=200,
         m=200,
         op=[ADD] * 20 + [RESET] * 2 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=100,
         n=500,
         m=500,
         op=[ADD] * 50 + [RESET] * 2 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=150,
         n=500,
         m=500,
         op=[ADD] * 50 + [RESET] * 2 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=40,
         n=10,
         m=10,
         op=[ADD] + [RESET] + [TRANSPOSE] * 5 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=40,
         n=100,
         m=100,
         op=[ADD] + [RESET] + [TRANSPOSE] * 20 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=80,
         n=200,
         m=200,
         op=[ADD] + [RESET] + [TRANSPOSE] * 20 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=100,
         n=500,
         m=500,
         op=[ADD] + [RESET] + [TRANSPOSE] * 50 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=100,
         n=500,
         m=500,
         op=[ADD] + [RESET] + [TRANSPOSE] * 50 + [OUTPUT] * 2
     ),
     dict(
+        dense=False,
         w=40,
         n=10,
         m=10,
         op=[ADD]  + [TRANSPOSE] + [OUTPUT] + [RESET] + [MULTIPLY] * 5
     ),
     dict(
+        dense=False,
         w=50,
         n=100,
         m=100,
         op=[ADD]  + [TRANSPOSE] + [OUTPUT] + [RESET] + [MULTIPLY] * 20
     ),
     dict(
+        dense=False,
         w=100,
         n=100,
         m=100,
         op=[ADD] + [TRANSPOSE] + [OUTPUT] + [RESET] + [MULTIPLY] * 50
     ),
     dict(
+        dense=False,
         w=100,
         n=500,
         m=500,
         op=[ADD] + [TRANSPOSE] + [OUTPUT] + [RESET] + [MULTIPLY] * 50
     ),
     dict(
+        dense=False,
         w=200,
         n=500,
         m=500,
         op=[ADD] + [TRANSPOSE] + [OUTPUT] + [RESET] + [MULTIPLY] * 50
+    ),
+    dict(
+        dense=True,
+        w=20,
+        n=5,
+        m=5,
+        op=[DENSE_ADD] * 1 + [DENSE_RESET] * 1 + [OUTPUT] *
+        5 + [TRANSPOSE] * 1 + [DENSE_MULTIPLY] * 20
+    ),
+    dict(
+        dense=True,
+        w=20,
+        n=20,
+        m=20,
+        op=[DENSE_ADD] * 1 + [DENSE_RESET] * 1 + [OUTPUT] *
+        5 + [TRANSPOSE] * 1 + [DENSE_MULTIPLY] * 50
+    ),
+    dict(
+        dense=True,
+        w=40,
+        n=40,
+        m=40,
+        op=[DENSE_ADD] * 1 + [DENSE_RESET] * 1 + [OUTPUT] *
+        5 + [TRANSPOSE] * 1 + [DENSE_MULTIPLY] * 50
+    ),
+    dict(
+        dense=True,
+        w=80,
+        n=80,
+        m=80,
+        op=[DENSE_ADD] * 1 + [DENSE_RESET] * 8 + [OUTPUT] *
+        5 + [TRANSPOSE] * 1 + [DENSE_MULTIPLY] * 50
+    ),
+    dict(
+        dense=True,
+        w=100,
+        n=100,
+        m=100,
+        op=[DENSE_ADD] * 1 + [DENSE_RESET] * 8 + [OUTPUT] *
+        5 + [TRANSPOSE] * 1 + [DENSE_MULTIPLY] * 50
     ),
 ]
--- src/lab5/generate_input.py
+++ src/lab5/generate_input.py
@@ -95,14 +95,6 @@ def add_operation(fb, matrix, ratio=0.95):
     sentence = "\n".join(items)
     fb.write(f"{sentence}\n")
 
-    # sentence = ""
-    # mat = np.random.normal(0, _DEVIATION, size=(
-    #     row_Q, col_Q)).astype(int).astype(str).tolist()
-    # mat = [" ".join(row) for row in mat]
-    # sentence = "\n".join(mat)
-    
-
-
 
 def transpose_operation(fb, matrix):
     temp = matrix.row
@@ -155,22 +147,151 @@ def multiply_operation(fb, matrix, ratio=0.95):
     sentence = "\n".join(items)
     fb.write(f"{sentence}\n")
 
-    # sentence = ""
-    # mat = np.random.normal(0, _DEVIATION, size=(
-    #     row_Q, col_Q)).astype(int).astype(str).tolist()
-    # mat = [" ".join(row) for row in mat]
-    # sentence = "\n".join(mat)
 
-    # sentence = "%d %d\n" % (row_Q, col_Q)
-    # fb.write(sentence)
+def add_operation(fb, matrix, ratio=0.95):
+    """
+    Args:
+        fb: file handler of the input test case
+        ratio: probability generating valid data
+        sparse_coef: coefficient to control the sparsity of the matrix Q
+    """
 
-    # sentence = ""
-    # mat = np.random.normal(0, _DEVIATION, size=(
-    #     row_Q, col_Q)).astype(int).astype(str).tolist()
-    # mat = [" ".join(row) for row in mat]
-    # sentence = "\n".join(mat)
+    sentence = "3\n"
+    fb.write(sentence)
 
-    # fb.write(sentence)
+    if random.random() < ratio:
+        row_Q = matrix.row
+        col_Q = matrix.col
+    else:
+        row_Q = random.randint(
+            matrix.row - 10 if matrix.row - 10 > 0 else matrix.row, matrix.row + 10)
+        col_Q = random.randint(
+            matrix.col - 10 > 0 if matrix.col - 10 > 0 else matrix.col, matrix.col + 10)
+
+    matrix.row = row_Q
+    matrix.col = col_Q
+
+    sentence = "%d %d\n" % (row_Q, col_Q)
+    fb.write(sentence)
+
+    num_non_zeros = random.randint(
+        (row_Q * col_Q) // 1000 + 1, (row_Q * col_Q) // 1000 + 10)
+
+    sentence = "%d\n" % num_non_zeros
+    fb.write(sentence)
+
+    items = []
+    item_indices = []
+    while num_non_zeros > 0:
+        row = random.randint(1, row_Q)
+        col = random.randint(1, col_Q)
+        if (row, col) not in item_indices:
+            item_indices.append((row, col))
+            val = random.randint(1, 10)
+            items.append((row, col, val))
+            num_non_zeros -= 1
+
+    items = sorted(items, key=lambda x: (x[0], x[1]))
+    items = [" ".join(str(v) for v in r) for r in items]
+    sentence = "\n".join(items)
+    fb.write(f"{sentence}\n")
+
+
+def dense_reset_operation(fb, matrix):
+    sentence = "1\n"
+    fb.write(sentence)
+
+    row_num = random.randint(
+        matrix.row-10 if matrix.row - 10 > 0 else matrix.row, matrix.row+10)
+    col_num = random.randint(
+        matrix.col-10 if matrix.col - 10 > 0 else matrix.col, matrix.col+10)
+    sentence = "%d %d\n" % (row_num, col_num)
+    fb.write(sentence)
+
+    matrix.row = row_num
+    matrix.col = col_num
+    mat = np.random.normal(0, 10000, size=(
+        matrix.row, matrix.col)).astype(int).astype(str).tolist()
+    mat = [" ".join(row) for row in mat]
+    sentence = "\n".join(mat)
+    fb.write(f"{sentence}\n")
+
+
+def dense_multiply_operation(fb, matrix, ratio=0.95):
+    sentence = "2\n"
+    fb.write(sentence)
+
+    if random.random() < ratio:
+        row_Q = matrix.row
+        col_Q = matrix.col
+    else:
+        row_Q = random.randint(
+            matrix.row - 10 if matrix.row - 10 > 0 else matrix.row, matrix.row + 10)
+        col_Q = random.randint(
+            matrix.col - 10 if matrix.col - 10 > 0 else matrix.col, matrix.col + 10)
+
+    matrix.row = row_Q
+    matrix.col = col_Q
+
+    sentence = "%d %d\n" % (row_Q, col_Q)
+    fb.write(sentence)
+
+    num_non_zeros = row_Q * col_Q
+    sentence = "%d\n" % num_non_zeros
+    fb.write(sentence)
+
+    items = []
+    for row in range(row_Q):
+        for col in range(col_Q):
+            val = random.randint(1, 3)
+            items.append((row+1, col+1, val))
+    
+    items = sorted(items, key=lambda x: (x[0], x[1]))
+    items = [" ".join(str(v) for v in r) for r in items]
+    sentence = "\n".join(items)
+    fb.write(f"{sentence}\n")
+
+
+def dense_add_operation(fb, matrix, ratio=0.95):
+    """
+    Args:
+        fb: file handler of the input test case
+        ratio: probability generating valid data
+        sparse_coef: coefficient to control the sparsity of the matrix Q
+    """
+
+    sentence = "3\n"
+    fb.write(sentence)
+
+    if random.random() < ratio:
+        row_Q = matrix.row
+        col_Q = matrix.col
+    else:
+        row_Q = random.randint(
+            matrix.row - 10 if matrix.row - 10 > 0 else matrix.row, matrix.row + 10)
+        col_Q = random.randint(
+            matrix.col - 10 > 0 if matrix.col - 10 > 0 else matrix.col, matrix.col + 10)
+
+    matrix.row = row_Q
+    matrix.col = col_Q
+
+    sentence = "%d %d\n" % (row_Q, col_Q)
+    fb.write(sentence)
+
+    num_non_zeros = row_Q * col_Q
+    sentence = "%d\n" % num_non_zeros
+    fb.write(sentence)
+
+    items = []
+    for row in range(row_Q):
+        for col in range(col_Q):
+            val = random.randint(1, 3)
+            items.append((row+1, col+1, val))
+
+    items = sorted(items, key=lambda x: (x[0], x[1]))
+    items = [" ".join(str(v) for v in r) for r in items]
+    sentence = "\n".join(items)
+    fb.write(f"{sentence}\n")
 
 
 def output_operation(fb, matrix):
@@ -184,6 +305,7 @@ def generate_input(path, settings):
     num_row = settings['n']
     num_col = settings['m']
     num_op = settings['w']
+    dense = settings['dense']
 
     fb.write("%d\n" % num_op)
 
@@ -194,9 +316,11 @@ def generate_input(path, settings):
     valid_operations = settings['op']
     for i in range(num_op):
         chosen_operation = random.choice(valid_operations)
-        if i == 0:
+        if i == 0 and dense is False:
             chosen_operation = "reset_operation"
-        if chosen_operation == "reset_operation":
+        if i == 0 and dense is True:
+            chosen_operation = "dense_reset_operation"
+        if chosen_operation == "reset_operation" or "dense_reset_operation":
             matrix.row = num_row
             matrix.col = num_col
         globals()[chosen_operation](fb, matrix)
@@ -211,7 +335,6 @@ def main():
     for case in tqdm(range(cfg.NUM_CASES)):
         input_filepath = 'data/exp5/input/input%d.txt' % case
         generate_input(input_filepath, cfg.PROBLEM_SETTINGS[case])
-
 
 
 if __name__ == '__main__':
